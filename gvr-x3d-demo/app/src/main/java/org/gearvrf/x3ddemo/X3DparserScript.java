f22ffb3264a8ef256e77326effd823dc929ff914
mwilliamsSamsung <m1.williams@partner.samsung.com>
2016-08-29 13:45:25 -0700

    X3d Exporter testing

--- gvr-x3d-demo/app/src/main/java/org/gearvrf/x3ddemo/X3DparserScript.java
+++ gvr-x3d-demo/app/src/main/java/org/gearvrf/x3ddemo/X3DparserScript.java
@@ -29,11 +29,16 @@ import org.gearvrf.GVRAndroidResource;
 //import org.gearvrf.GVRBaseSensor;
 import org.gearvrf.GVRCameraRig;
 import org.gearvrf.GVRContext;
+import org.gearvrf.GVRCursorController;
 import org.gearvrf.GVRDirectLight;
 import org.gearvrf.GVREventReceiver;
 
+import org.gearvrf.GVRMesh;
 import org.gearvrf.animation.GVRAnimation;
 import org.gearvrf.animation.GVRAnimationEngine;
+import org.gearvrf.io.GVRControllerType;
+import org.gearvrf.io.GVRInputManager;
+import org.gearvrf.scene_objects.GVRCubeSceneObject;
 import org.gearvrf.scene_objects.GVRModelSceneObject;
 import org.gearvrf.GVRScene;
 import org.gearvrf.GVRSceneObject;
@@ -52,14 +57,16 @@ import android.os.Environment;
 import android.util.Log;
 
 import org.gearvrf.x3d.*;
+import org.joml.Matrix4f;
+import org.joml.Vector3f;
 
 /**
- * 
- * @author m1.williams 
+ *
+ * @author m1.williams
  * X3d Demo comes with test files in the assets directory:
  *         cylindersandplanes.x3d, helloworldtext.x3d, multipleanimations.x3d,
  *         multiplepointlights.x3d, navigationinfo.x3d, teapotandtorus.x3d
- * 
+ *
  *         In the line: "String filename = ", change to one of the above file
  *         names or supply you own.
  *
@@ -68,26 +75,29 @@ import org.gearvrf.x3d.*;
 public class X3DparserScript extends GVRScript
 {
 
-  private static final String TAG = "X3D Parser Script";
+  private static final String TAG = X3DparserScript.class.getSimpleName();
   private GVRContext mGVRContext = null;
 
-  public GVRAnimationEngine mAnimationEngine;
-  public List<GVRAnimation> mAnimations = new ArrayList<GVRAnimation>();
+  //public GVRAnimationEngine mAnimationEngine;
+  //public List<GVRAnimation> mAnimations = new ArrayList<GVRAnimation>();
 
-  X3Dobject x3dObject = null;
-  GVRScene scene = null;
+  //X3Dobject x3dObject = null;
+  private GVRScene scene = null;
 
-  public GVRSceneObject currentPickedObject = null;
-  public boolean tappedObject = false;
+  //public GVRSceneObject currentPickedObject = null;
+  private X3DparserActivity activity;
 
   public X3DparserScript(X3DparserActivity activity)
   {
+    this.activity = activity;
   }
 
   public void onInit(GVRContext gvrContext)
   {
     mGVRContext = gvrContext;
 
+    scene = mGVRContext.getNextMainScene();
+    /*
     scene = gvrContext.getNextMainScene(new Runnable()
     {
       @Override
@@ -95,52 +105,102 @@ public class X3DparserScript extends GVRScript
       {
         for (GVRAnimation animation : mAnimations)
         {
-          animation.start(mAnimationEngine);
+          //Not required
+          //animation.start(mAnimationEngine);
         }
       }
     });
+    */
 
-    mAnimationEngine = gvrContext.getAnimationEngine();
+    //mAnimationEngine = gvrContext.getAnimationEngine();
+
     scene.getMainCameraRig().getLeftCamera().setBackgroundColor(Color.BLACK);
     scene.getMainCameraRig().getRightCamera().setBackgroundColor(Color.BLACK);
 
     GVRModelSceneObject model = new GVRModelSceneObject(mGVRContext);
     // X3D test files should be in the assets directory.
     // Replace 'filename' to view another .x3d file
-    String filename = "navigationinfo.x3d";
+    //String filename = "touchSensor.x3d";
+    //String filename = "touchSensor5.x3d";
+    //String filename = "multiviewpoints01.x3d";
+    String filename = "exportPlane.x3d";
+    //String filename = "torusrainbow.x3d";
+    //String filename = "torusrainbownonormals.x3d";
+    //String filename = "teapotandtorusnonormals.x3d";
+    //String filename = "teapotandtoruspointlights.x3d";
+    //String filename = "teapotandtorusMinTransform.x3d";
+    //String filename = "teapotandtorusExport.x3d";
+
     try
     {
       GVRCameraRig mainCameraRig = scene.getMainCameraRig();
 
       model = gvrContext.getAssetLoader().loadModel(filename, scene);
-      List<GVRAnimation> animations = model.getAnimations();
-      mAnimations = animations;
+      GVRSceneObject x3dCamera = model.getSceneObjectByName("MainCamera");
+      GVRCameraRig x3dCameraRig = x3dCamera.getCameraRig();
+      GVRTransform x3dCameraTrans = x3dCamera.getTransform();
+      Matrix4f x3dCameraMatrix = x3dCameraTrans.getLocalModelMatrix4f();
+      Matrix4f modelMatrix = model.getTransform().getModelMatrix4f();
+      Vector3f modelPos = new Vector3f();
+      Vector3f camPos = new Vector3f();
+      //List<GVRAnimation> animations = model.getAnimations();
+      int backgroundColor = x3dCameraRig.getLeftCamera().getBackgroundColor();
 
-      GVRTransform newtrans = model.getCameraRig().getTransform();
-
-      int backgroundColor = model.getCameraRig().getLeftCamera()
-          .getBackgroundColor();
+      //mAnimations = animations;
       mainCameraRig.getLeftCamera().setBackgroundColor(backgroundColor);
       mainCameraRig.getRightCamera().setBackgroundColor(backgroundColor);
+      mainCameraRig.setNearClippingDistance(x3dCameraRig.getNearClippingDistance());
+      mainCameraRig.setFarClippingDistance(x3dCameraRig.getFarClippingDistance());
+      mainCameraRig.getTransform().setModelMatrix(x3dCameraMatrix);
 
-      GVRTransform mainCameraRigTransform = mainCameraRig.getTransform();
-      mainCameraRigTransform.setPosition(newtrans.getPositionX(),
-                                         newtrans.getPositionY(),
-                                         newtrans.getPositionZ());
+      /*
+      // if the x3D camera is on top of the model
+      // reposition the camera
+      modelMatrix.getTranslation(modelPos);
+      x3dCameraMatrix.getTranslation(camPos);
+      if (modelPos.distance(camPos) < 0.001f)
+      {
+        GVRSceneObject.BoundingVolume bv = model.getBoundingVolume();
+        float sf = 1 / bv.radius;
+
+        model.getTransform().setScale(sf, sf, sf);
+        Vector3f pos = new Vector3f(-bv.center.x, -bv.center.y, -bv.center.z);
+        pos.mul(sf);
+        model.getTransform().setPosition(pos.x, pos.y, pos.z - 4.0f);
+      }
+      */
+
+/*  Didn't copy the cursor to this branch, but don't need it for the moment
+      GVRSceneObject cursor = new GVRSceneObject(mGVRContext,
+              new FutureWrapper<GVRMesh>(mGVRContext.createQuad(1.0f, 1.0f)),
+              mGVRContext.loadFutureTexture(new GVRAndroidResource(mGVRContext, R.raw.cursor)));
+      cursor.getTransform().setPosition(0.0f, 0.0f, -10.0f);
+      cursor.getRenderData().setDepthTest(false);
+      cursor.getRenderData().setRenderingOrder(100000);
+
+      mainCameraRig.addChildObject(cursor);
+*/
 
       // check if a headlight was attached to the model's camera rig
       // during parsing, as specified by the NavigationInfo node.
-      // If 4 objects are attached to the camera rig, one must be the
-      // directionalLight. Thus attach a dirLight to the main camera
-      if (model.getCameraRig().getChildrenCount() > 3)
+      GVRSceneObject headLightSceneObject = model.getSceneObjectByName("HeadLight");
+      if (headLightSceneObject != null)
       {
-        GVRSceneObject headlightSceneObject = new GVRSceneObject(gvrContext);
-        headlightSceneObject.setName("headlightSceneObject");
-        GVRDirectLight headLight = new GVRDirectLight(gvrContext);
-        headlightSceneObject.attachLight(headLight);
-        headLight.setDiffuseIntensity(1, 1, 1, 1);
-        mainCameraRig.addChildObject(headlightSceneObject);
+        headLightSceneObject.getParent().removeChildObject(headLightSceneObject);
+        mainCameraRig.addChildObject(headLightSceneObject);
       }
+
+
+      try {
+        Log.e("Camera", "setMainCameraRig(x3dCameraRig)" );
+        if (x3dCameraRig == null ) Log.e("Camera", "x3dCameraRig == null" );
+        //scene.setMainCameraRig(x3dCameraRig);
+        Log.e("Camera", "DONE setMainCameraRig(x3dCameraRig)" );
+      }
+      catch  (Exception e){
+        Log.e("Camera", "exception " + e);
+      }
+
 
     }
     catch (FileNotFoundException e)
@@ -151,10 +211,40 @@ public class X3DparserScript extends GVRScript
     {
       Log.d(TAG, "Error IOException = " + e);
     }
+    catch (NullPointerException e)
+    {
+      Log.d(TAG, "Null Pointer Exception = " + e);
+    }
     catch (Exception e)
     {
+      Log.d(TAG, "Exception = " + e);
       e.printStackTrace();
     }
+
+    Log.e("Camera", "Before GVRInputManager inputManager" );
+
+    /*  commented out when I didn't have the cursor icon
+    GVRInputManager inputManager = mGVRContext.getInputManager();
+    for (GVRCursorController controller : inputManager.getCursorControllers()) {
+      if (controller.getControllerType() == GVRControllerType.GAZE) {
+        controller.setPosition(0.0f, 0.0f, -10.0f);
+      }
+    }
+    Log.e("Camera", "AFTER GVRInputManager inputManager" );
+    */
+
+    /*
+    try {
+      AssetManager am = activity.getAssets();
+      String[] amList = am.list("");
+      Log.e(TAG, "amList length "+ amList.length );
+    }
+    catch (IOException e) {
+      Log.e(TAG, "AssetManager list exception" );
+    }
+    */
+    X3Dexporter x3dExporter = new X3Dexporter(gvrContext, filename, model);
+
   } // end onInit()
 
   // @Override
@@ -182,7 +272,7 @@ public class X3DparserScript extends GVRScript
             if (lastScreenshotCenterFinished)
             {
               mGVRContext
-                  .captureScreenCenter(newScreenshotCallback(filename, 0));
+                      .captureScreenCenter(newScreenshotCallback(filename, 0));
               lastScreenshotCenterFinished = false;
             }
             break;
@@ -197,7 +287,7 @@ public class X3DparserScript extends GVRScript
             if (lastScreenshotRightFinished)
             {
               mGVRContext
-                  .captureScreenRight(newScreenshotCallback(filename, 2));
+                      .captureScreenRight(newScreenshotCallback(filename, 2));
               lastScreenshotRightFinished = false;
             }
             break;
@@ -216,7 +306,7 @@ public class X3DparserScript extends GVRScript
   }
 
   private GVRScreenshotCallback newScreenshotCallback(final String filename,
-      final int mode)
+                                                      final int mode)
   {
     return new GVRScreenshotCallback()
     {
@@ -227,7 +317,7 @@ public class X3DparserScript extends GVRScript
         if (bitmap != null)
         {
           File file = new File(Environment.getExternalStorageDirectory(),
-              filename + ".png");
+                  filename + ".png");
           FileOutputStream outputStream = null;
           try
           {
@@ -287,7 +377,7 @@ public class X3DparserScript extends GVRScript
           {
             Bitmap bitmap = bitmapArray[i];
             File file = new File(Environment.getExternalStorageDirectory(),
-                filename + "_" + i + ".png");
+                    filename + "_" + i + ".png");
             FileOutputStream outputStream = null;
             try
             {
